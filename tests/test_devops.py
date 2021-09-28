002d57b5d699bacf04e3a0d62afa5d09e8808d53
Tamiris Cristiane Steimbch <tcsteimbch@gmail.com>
2021-09-28 20:44:40 -0300

    random and coverage added

--- tests/test_devops.py
+++ tests/test_devops.py
@@ -1,6 +1,7 @@
 import pytest
 import importlib.util
 import importlib.machinery
+import random
 
 # import and load modules
 loader = importlib.machinery.SourceFileLoader('devops', './pages/devops.py')
@@ -16,18 +17,31 @@ class TestDevopsApi:
         response = self.devopsApi.create_new_vehicle("honda", "civic", "silver", "2009-03-02T13:23:41.12")
         assert response.status_code == 201
 
-    def test_find_vehicle(self):
-        response = self.devopsApi.find_vehicle(1)
-        assert response.status_code == 200
-
     def test_get_vehicles(self):
         response = self.devopsApi.get_vehicles()
         assert response.status_code == 200
-    
+
+    def test_find_vehicle(self):
+        existing_ids = [
+            id["id"]
+            for id in self.devopsApi.get_vehicles().json()
+        ]
+        response = self.devopsApi.find_vehicle(random.choice(existing_ids))
+        assert response.status_code == 200
+
     def test_update_vehicle(self):
-        response = self.devopsApi.update_vehicle("hondaUpdated", "civicUpdated", "silverUpdated", "2021-08-20T13:22:22.22", 1)
+        existing_ids = [
+            id["id"]
+            for id in self.devopsApi.get_vehicles().json()
+        ]
+        response = self.devopsApi.update_vehicle("hondaUpdated", "civicUpdated", "silverUpdated", 
+                                                "2021-08-20T13:22:22.22", random.choice(existing_ids))
         assert response.status_code == 200
      
     def test_delete_vehicle(self):
-        response = self.devopsApi.delete_vehicle(1)
+        existing_ids = [
+            id["id"]
+            for id in self.devopsApi.get_vehicles().json()
+        ]
+        response = self.devopsApi.delete_vehicle(random.choice(existing_ids))
         assert response.status_code == 204
